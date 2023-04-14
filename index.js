7e375a34b713ef822ca14955b569cc8de5c54c41
elborracho420 <e.baxter@dark-coin.io>
2023-04-14 18:25:46 -0500

    Animated player to move down y axis and stop at bottom of canvas.

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 19;
 canvas.height = 64 * 9;
 
-c.fillStyle = 'white';
-c.fillRect(0, 0, canvas.width, canvas.height);
+let y = 100;
+const height = 100;
+let bottom = y + height;
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    c.fillStyle = 'red';
+    c.fillRect(100, y, 100, height);
+
+    if (bottom < canvas.height) {
+        y++;
+        bottom = y + 100;
+    }
+}
+
+animate();
