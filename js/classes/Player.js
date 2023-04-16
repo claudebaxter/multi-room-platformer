abe90ee50825313809af8cabe6e0fc5c9472f798
elborracho420 <e.baxter@dark-coin.io>
2023-04-16 18:15:51 -0500

    Rendered and animated king idle sprite at player location.

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,7 +1,10 @@
-class Player {
+class Player extends Sprite {
     constructor({
-        collisionBlocks = []
+        collisionBlocks = [],
+        imageSrc,
+        frameRate
     }) {
+        super({imageSrc, frameRate})
         this.position = {
             x: 200,
             y: 200
@@ -10,8 +13,6 @@ class Player {
             x: 0,
             y: 0
         }
-        this.width = 25
-        this.height = 25
         this.sides = {
             bottom: this.position.y + this.height
         }
@@ -19,11 +20,9 @@ class Player {
 
         this.collisionBlocks = collisionBlocks
     }
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
     update() {
+        //c.fillStyle = 'rgba(0, 0, 255, 0.5)'
+        //c.fillRect(this.position.x, this.position.y, this.width, this.height)
         this.position.x += this.velocity.x
         this.checkForHorizontalCollisions();
         this.applyGravity();
