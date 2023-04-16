ecae7a6f11b0a99b619b963fcb57b5a78aa135eb
elborracho420 <e.baxter@dark-coin.io>
2023-04-16 16:47:27 -0500

    Adding horizontal collision block / player detection and stopping player movement.

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,5 +1,7 @@
 class Player {
-    constructor() {
+    constructor({
+        collisionBlocks = []
+    }) {
         this.position = {
             x: 100,
             y: 100
@@ -14,6 +16,8 @@ class Player {
             bottom: this.position.y + this.height
         }
         this.gravity = 1
+
+        this.collisionBlocks = collisionBlocks
     }
     draw() {
         c.fillStyle = 'red';
@@ -21,6 +25,27 @@ class Player {
     }
     update() {
         this.position.x += this.velocity.x
+        //check for horizontal collisions
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i];
+
+            //if a collision exists
+            if (this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.position.x + this.width >= collisionBlock.position.x &&
+                this.position.y + this.height >= collisionBlock.position.y &&
+                this.position.y <= collisionBlock.position.y + collisionBlock.height) {
+                    //collision on x axis going left
+                    if (this.velocity.x < -1) {
+                        this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01;
+                        break;
+                    }
+                    if (this.velocity.x > 1) {
+                        this.position.x = collisionBlock.position.x - this.width - 0.01;
+                        break;
+                    }
+                }
+        }
+
         this.position.y += this.velocity.y;
         this.sides.bottom = this.position.y + this.height;
         //above bottom of canvas
